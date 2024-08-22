bc46dee3a063c4d19af5d0d3ab5abe036efa2bf5
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2024-08-22 14:19:51 +0100

    add Response to Facebook Post Policy

--- app/Policies/Channels/Facebook/FacebookPostPolicy.php
+++ app/Policies/Channels/Facebook/FacebookPostPolicy.php
@@ -2,8 +2,9 @@
 
 namespace App\Policies\Channels\Facebook;
 
-use App\Models\FacebookPost;
 use App\Models\User;
+use App\Models\FacebookPost;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class FacebookPostPolicy
@@ -12,19 +13,19 @@ class FacebookPostPolicy
 
     public function duplicatePost(User $user, FacebookPost $facebookPost)
     {
-        return $user->id === $facebookPost->facebook_page->user_id;
+        return $user->id === $facebookPost->facebook_page->user_id ? Response::allow() : Response::deny("You're not authorized to duplicate this post");
     }
 
     public function viewPostAnalytics(User $user, FacebookPost $facebookPost)
     {
-        return $user->id === $facebookPost->facebook_page->user_id;
+        return $user->id === $facebookPost->facebook_page->user_id ? Response::allow() : Response::deny("You're not authorized to view the analytics of this post");
     }
 
     public function updatePost(User $user, FacebookPost $facebookPost) {
-        return $user->id === $facebookPost->facebook_page->user_id;
+        return $user->id === $facebookPost->facebook_page->user_id ? Response::allow() : Response::deny("You're not authorized to update this post");
     }
 
     public function deletePost(User $user, FacebookPost $facebookPost) {
-        return $user->id === $facebookPost->facebook_page->user_id;
+        return $user->id === $facebookPost->facebook_page->user_id ? Response::allow() : Response::deny("You're not authorized to delete this post");
     }
 }
