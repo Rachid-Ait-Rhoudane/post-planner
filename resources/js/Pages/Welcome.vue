39cc0e362bd18e2b7d64940208f85a2d7714dc56
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2024-09-14 13:48:19 +0100

    add about us section to welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,9 +19,9 @@ let showMobileMenu = ref(false);
     <nav class="bg-gray-100 shadow-md">
 
         <div class="max-w-7xl mx-auto flex justify-between items-center px-3 py-5">
-            <img class="w-20 sm:w-28" src="https://post-planner.ca/images/POPlanner.png" alt="app logo">
+            <img class="w-20 sm:w-28" src="https://poplanner.online/images/POPlanner.png" alt="app logo">
             <div class="hidden sm:flex sm:items-center sm:gap-4 md:gap-10">
-                <a class="text-gray-500 font-bold hover:underline hover:text-gray-400" href="#">About Us</a>
+                <a class="text-gray-500 font-bold hover:underline hover:text-gray-400" href="#about_us">About Us</a>
                 <a class="text-gray-500 font-bold hover:underline hover:text-gray-400" href="#">Pricing</a>
                 <a class="text-gray-500 font-bold hover:underline hover:text-gray-400" href="#">Contact</a>
             </div>
@@ -52,7 +52,7 @@ let showMobileMenu = ref(false);
                 >
                     <div v-show="showMobileMenu" class="absolute z-20 right-0 bg-white rounded-md border border-gray-100 shadow-md min-w-52 py-2">
                         <div class="flex flex-col md:gap-10">
-                            <a class="text-gray-500 font-bold p-2 hover:bg-indigo-500 hover:text-white" href="#">About Us</a>
+                            <a class="text-gray-500 font-bold p-2 hover:bg-indigo-500 hover:text-white" href="#about_us">About Us</a>
                             <a class="text-gray-500 font-bold p-2 hover:bg-indigo-500 hover:text-white" href="#">Pricing</a>
                             <a class="text-gray-500 font-bold p-2 hover:bg-indigo-500 hover:text-white" href="#">Contact</a>
                         </div>
@@ -100,7 +100,7 @@ let showMobileMenu = ref(false);
                 </p>
             </div>
 
-            <img class="w-[450px] aspect-video" src="https://post-planner.ca/images/social_media_channels.png" alt="social media channels">
+            <img class="w-[450px] aspect-video" src="https://poplanner.online/images/social_media_channels.png" alt="social media channels">
         </div>
 
         <div class="mt-32">
@@ -118,7 +118,7 @@ let showMobileMenu = ref(false);
                         Start Connecting Your Channels
                     </Link>
                 </div>
-                <img class="hidden lg:block" src="https://post-planner.ca/images/connect_social_media_channels.png" alt="connect social media channels">
+                <img class="hidden lg:block" src="https://poplanner.online/images/connect_social_media_channels.png" alt="connect social media channels">
             </div>
 
             <div class="bg-gray-100">
@@ -131,7 +131,7 @@ let showMobileMenu = ref(false);
                             Start Posting To Your Channels
                         </Link>
                     </div>
-                    <img class="hidden lg:block lg:w-[435px]" src="https://post-planner.ca/images/post_to_social_media_channels.png" alt="post to social media channels">
+                    <img class="hidden lg:block lg:w-[435px]" src="https://poplanner.online/images/post_to_social_media_channels.png" alt="post to social media channels">
                 </div>
             </div>
 
@@ -144,14 +144,14 @@ let showMobileMenu = ref(false);
                         Start Scheduling Your Posts
                     </Link>
                 </div>
-                <img class="hidden lg:block lg:w-[450px] lg:rounded-full" src="https://post-planner.ca/images/schedule_posts.png" alt="schedule posts">
+                <img class="hidden lg:block lg:w-[450px] lg:rounded-full" src="https://poplanner.online/images/schedule_posts.png" alt="schedule posts">
             </div>
 
         </div>
 
-        <div class="relative my-16 h-[500px] overflow-hidden flex items-center">
+        <div id="about_us" class="relative my-16 h-[500px] overflow-hidden flex items-center">
             <img class="absolute top-0 left-0 w-full h-full object-cover object-right md:object-center" src="https://poplanner.online/images/about_us.png" alt="about us">
-            <div class="absolute top-0 left-0 w-full h-full bg-black/30"></div>
+            <div class="absolute top-0 left-0 w-full h-full bg-black/70 md:bg-black/30"></div>
             <div class="relative max-w-7xl mx-auto px-3 py-16 text-white flex flex-col gap-12">
                 <h1 class="text-4xl font-black">About Us</h1>
                 <p class="w-full md:w-[65%] leading-7 font-bold">POPlanner is designed to simplify social media management for everyone. Our app lets you easily connect your social channels, schedule posts, and track basic analytics like reactions, comments, and shares—all in one place. With just a few clicks, you can automate your posts and monitor engagement, saving time and helping you grow your online presence more efficiently.</p>
