7dd030027101335e96bd53128a8c4e5cd65640cb
Rachid-Ait-Rhoudane <rachid2aitrhoudane@gmail.com>
2024-09-19 13:02:57 +0100

    add static dashboard

---
+++ public/build/assets/AuthenticationCardLogo-DkIoJ-ig.js
@@ -0,0 +1,1 @@
+import{o as a,c as o,w as e,b as t,u as r,i as s}from"./app-DpONCW-f.js";import{A as c}from"./ApplicationLogo-zvEkvWYD.js";const f={__name:"AuthenticationCardLogo",setup(i){return(n,_)=>(a(),o(r(s),{href:"/"},{default:e(()=>[t(c,{class:"w-44 h-20"})]),_:1}))}};export{f as _};
--- public/build/assets/AuthenticationCardLogo-d-MJgdhO.js
+++
@@ -1,1 +0,0 @@
-import{o as a,c as o,w as e,b as t,u as r,i as s}from"./app-DKGYE72o.js";import{A as c}from"./ApplicationLogo-Bsya1R0c.js";const f={__name:"AuthenticationCardLogo",setup(i){return(n,_)=>(a(),o(r(s),{href:"/"},{default:e(()=>[t(c,{class:"w-44 h-20"})]),_:1}))}};export{f as _};
